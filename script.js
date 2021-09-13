6dece3de059de8a121accf7b0c853d917c7b4ced
Gorz <GorzD3v@gmail.com>
2021-09-13 17:31:00 -0300

    Tamaño popup en MQ

--- script.js
+++ script.js
@@ -20,12 +20,12 @@ function computerPlay() {
   let resultado = Math.floor(Math.random() * 3) + 1;
 
   if (resultado == 3) {
-    document.getElementById("pc").src = "/images/pcPiedra.png";
+    document.getElementById("pc").src = "../images/pcPiedra.png";
     return "Piedra";
   } else if (resultado == 2) {
-    document.getElementById("pc").src = "/images/pcPapel.png";
+    document.getElementById("pc").src = "../images/pcPapel.png";
     return "Papel";
-  } else document.getElementById("pc").src = "/images/pcTijera.png";
+  } else document.getElementById("pc").src = "../images/pcTijera.png";
   return "Tijera";
 }
 
@@ -111,11 +111,11 @@ papel.addEventListener("click", () => game("Papel", computerPlay()));
 tijera.addEventListener("click", () => game("Tijera", computerPlay()));
 
 piedra.addEventListener("click", () => {
-  document.getElementById("player").src = "/images/playerPiedra.png";
+  document.getElementById("player").src = "../images/playerPiedra.png";
 });
 papel.addEventListener("click", () => {
-  document.getElementById("player").src = "/images/playerPapel.png";
+  document.getElementById("player").src = "../images/playerPapel.png";
 });
 tijera.addEventListener("click", () => {
-  document.getElementById("player").src = "/images/playerTijera.png";
+  document.getElementById("player").src = "../images/playerTijera.png";
 });
